00f74482963e93e1573bbaf4f6fd4d5a2ab3c8a5
Bogdan <bogdan.oloeriu@gmail.com>
2020-02-07 00:41:52 +0100

    persisted document in firestore

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,18 +2,29 @@ package com.example.demo;
 
 
 import com.example.demo.model.Book;
+import com.example.demo.service.DataService;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.WriteResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Inject;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
 
 @RestController
 public class HelloController {
 
     private static Logger logger = Logger.getLogger(HelloController.class.getName());
+
+    @Inject
+    private DataService dataService;
 
     @RequestMapping("/")
     public String index() {
@@ -26,8 +37,35 @@ public class HelloController {
         Date date = new Date();
         Book demoBook = new Book("demoBook " + date.toString(), "cezar", 2020);
 
-
         return demoBook;
     }
 
+    @GetMapping("/testInjectedService")
+    public @ResponseBody
+    Book testInjectedService() {
+
+        return dataService.getBook();
+    }
+
+    @GetMapping("/quickstartServer")
+    public @ResponseBody
+    String quickstartServer() throws ExecutionException, InterruptedException {
+
+
+        DocumentReference docRef = dataService.getFirestoreService()
+                .collection("users").document("alovelace");
+        // Add document data  with id "alovelace" using a hashmap
+        Map<String, Object> data = new HashMap<>();
+        data.put("first", "Ada");
+        data.put("last", "Lovelace");
+        data.put("born", 1815);
+
+        //asynchronously write data
+        ApiFuture<WriteResult> result = docRef.set(data);
+
+        // ...
+        // result.get() blocks on response
+        System.out.println("Update time : " + result.get().getUpdateTime());
+        return "Update time = " + result.get().getUpdateTime();
+    }
 }
