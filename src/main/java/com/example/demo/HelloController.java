4cdd82b8247bcf10610528be92896b3345e6afe5
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2020-02-06 19:07:28 +0100

    succesful persisted entity in datastore

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,15 +2,24 @@ package com.example.demo;
 
 
 import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 public class HelloController {
+
+    private static Logger logger = Logger.getLogger(HelloController.class.getName());
+
+    @Autowired
+    BookRepository bookRepository;
 
     @RequestMapping("/")
     public String index() {
@@ -22,6 +31,10 @@ public class HelloController {
     Book testBookCreateReadAndReturn() {
         Date date = new Date();
         Book demoBook = new Book("demoBook " + date.toString(), "bogdan", 2020);
+        Book savedBook = this.bookRepository.save(demoBook);
+
+
+        logger.log(Level.INFO, "Hello from logger: DemoBook id = " + demoBook.getId());
         return demoBook;
     }
 
