cbb78d505729b04940638c883eaad361432019f1
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2020-02-06 19:27:56 +0100

    implemented examples for looking up items in datastore

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -30,11 +31,34 @@ public class HelloController {
     public @ResponseBody
     Book testBookCreateReadAndReturn() {
         Date date = new Date();
-        Book demoBook = new Book("demoBook " + date.toString(), "bogdan", 2020);
+        Book demoBook = new Book("demoBook " + date.toString(), "cezar", 2020);
         Book savedBook = this.bookRepository.save(demoBook);
 
 
         logger.log(Level.INFO, "Hello from logger: DemoBook id = " + demoBook.getId());
+
+        Iterable<Book> books = this.bookRepository.findAll();
+        int i = 0;
+        logger.log(Level.INFO, "All Books");
+        for (Book book : books) {
+            logger.log(Level.INFO, "" + ++i + book);
+        }
+
+        logger.log(Level.INFO, "Found books");
+        List<Book> foundBooks = this.bookRepository.findByAuthor("cezar");
+        foundBooks.stream().forEach(book -> logger.log(Level.INFO, book.toString()));
+
+        List<Book> publishedAfterYer = this.bookRepository.findByYearGreaterThan(2000);
+        logger.log(Level.INFO, "Books published after year 2000");
+        publishedAfterYer.stream().forEach(book -> logger.log(Level.INFO, book.toString()));
+
+        List<Book> byAuthorAndYear = this.bookRepository.findByAuthorAndYear("cezar", 2020);
+        logger.log(Level.INFO, "Books published by cezar in 2000");
+        byAuthorAndYear.stream().forEach(book -> logger.log(Level.INFO, book.toString()));
+
+        //delete example
+        //this.bookRepository.deleteAll();
+
         return demoBook;
     }
 
