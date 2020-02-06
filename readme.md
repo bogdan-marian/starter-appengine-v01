cfa158702e9e00a4ce5bffa7d66ba52acc570275
bogdan <bogdan.oloeriu@around.media>
2020-02-06 15:28:21 +0100

    good serialization and deserialization

--- readme.md
+++ readme.md
@@ -5,5 +5,9 @@
    - Second step and more complete [using gradle and appengine plugin](https://cloud.google.com/appengine/docs/flexible/java/using-gradle)
 - test locally: Inspired from:  [Building an Application with Spring Boot](https://spring.io/guides/gs/spring-boot/)
 
-# integrate firestore
+## integrate firestore
 - [quick start server client library](https://cloud.google.com/firestore/docs/quickstart-servers)
+
+## datastore
+### spring boot datastore tutorial
+- [spring boot datastore tutorial](https://codelabs.developers.google.com/codelabs/cloud-spring-datastore/index.html?index=..%2F..index#0)
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
 
+import com.example.demo.model.Book;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class HelloController {
@@ -10,6 +15,14 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/testBookCreateReadAndReturn")
+    public @ResponseBody
+    Book testBookCreateReadAndReturn() {
+        Date date = new Date();
+        Book demoBook = new Book("demoBook " + date.toString(), "bogdan", 2020);
+        return demoBook;
     }
 
 }
